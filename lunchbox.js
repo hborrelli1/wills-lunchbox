a7ce9cff96a0d4ff02d894a34f2121923ac78e53
Harry Borrelli <hborrelli@outlook.com>
2019-12-21 15:05:10 -0700

    complete challenge

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,25 @@
 class LunchBox {
+  constructor(lunchBoxInfo) {
+    this.owner = lunchBoxInfo.owner;
+    this.material = lunchBoxInfo.madeOf;
+    this.shape = lunchBoxInfo.shape;
+    this.color = lunchBoxInfo.color;
+    this.handle = true;
+    this.snacks = [];
+    this.healthySnacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+    if (snack.healthy === true) {
+      this.healthySnacks.push(snack);
+    }
+  }
+
+  findHealthySnacks() {
+    return this.healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- snack.js
+++ snack.js
@@ -1,5 +1,17 @@
 class Snack {
+  constructor(snack) {
+    this.deliciousLevel = 'extra';
+    this.type = snack;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    snack.includes('Fruit') ? this.healthy = true : this.healthy = false;
+  }
 
+  getEaten() {
+    this.amount -= 10;
+    this.amount <= 20 ? this.cuttingItClose = true : this.cuttingItClose = false;
+  }
 }
 
 module.exports = Snack;
